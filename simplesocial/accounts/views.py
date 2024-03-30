b56c747644b98de1d256eec8ecd39b7ef648ff12
meetdomadiya <meet.do1998@gmail.com>
2024-03-30 13:33:05 +0100

    Login/Logout URLS and migrations

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -6,5 +6,5 @@ from django.views.generic import CreateView
 # Create your views here.
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy('login')
     template_name = "accounts/signup.html"
+    success_url = reverse_lazy('accounts:login')
