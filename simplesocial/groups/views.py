a6b2f9e9420c75c6622141a4f802a2b6cdf19f55
meetdomadiya <meet.do1998@gmail.com>
2024-04-04 13:11:58 +0200

    created views for groups templates and connected templates to those files

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
 
+from groups.models import Group,GruopMember
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description') #user can edit this
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroup(generic.ListView):
+    model = Group
+
