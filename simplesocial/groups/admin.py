75bf1648c40b68fa621c6736a4dcb95519148e41
meetdomadiya <meet.do1998@gmail.com>
2024-05-04 12:00:44 +0200

    connect the views with urls, register groups model in admin.py

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GruopMember
+
+admin.site.register(models.Group)
---
+++ simplesocial/groups/urls.py
@@ -0,0 +1,12 @@
+#Groups urls.py file
+from django.urls import path
+from . import views
+
+app_name = 'groups'
+
+urlpatterns = [
+    path(r'^$', views.ListGroup.as_view(),name = 'all'),
+    path(r'^new/$',views.CreateGroup.as_view(),name='create'),
+    path(r'posts/in/(?P<slug>[-\w+])/$',views.SingleGroup.as_view(),name='single'),
+
+]
